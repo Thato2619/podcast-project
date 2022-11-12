a3396fde43630c385ed8e1f8876b9ad44ab5aa22
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-12 10:04:49 +0200

    add htmlList to list out all podcasts in APi .
    Use async function to load all needed podcast info before fetching the API

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 import './components/api.js'
+
+//manipulate the dom 
+
+const htmlList = document.querySelector('#list')
+
+//add async function to get all podcast needed
+const getAllPodcasts = async () => {
+    htmlList.innerHTML = `Loading...`
+}
