f22bc16a074629319ff133ae23de66cb57335e7e
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-01-06 09:25:08 +0200

    add styling in title and title paragraph and seasons styling
    - Don't forget to indent the into the styling css

--- components/podcast-preview.js
+++ components/podcast-preview.js
@@ -12,38 +12,55 @@ class Component extends LitElement{
     }
      //add styling 
      static styles = css`
-     * {
-        box-sizing: border-box;
-     }
+        * {
+            box-sizing: border-box;
+        }
 
-     .wrapper {
-         max-width: 100%;
-         min-height:20rem;
-         margin-top:3px;
-         margin-left:3px;
-         margin-right:3px;
-         display:flex;
-         background-color: cadetblue;
-         color: #EEE;
-         border-radius: 5px;
-         position:relative;
-         z-index: 1;
-     }
+        .wrapper {
+            max-width: 100%;
+            min-height:20rem;
+            margin-top:3px;
+            margin-left:3px;
+            margin-right:3px;
+            display:flex;
+            background-color: cadetblue;
+            color: #EEE;
+            border-radius: 5px;
+            position:relative;
+            z-index: 1;
+        }
 
-     .image{
-         height:10rem;
-         width:10rem;
-     }
+        .image{
+            height:10rem;
+            width:10rem;
+        }
 
-     .podcastImage .img{
-         width:10rem;
-         height: 10rem;
-         border-bottom-left-radius:5px;
-         border-top-left-radius:5px;
-         object-fit:cover;
-         position:absolute;
-     }
-     `;
+        .podcastImage .img{
+            width:10rem;
+            height: 10rem;
+            border-bottom-left-radius:5px;
+            border-top-left-radius:5px;
+            object-fit:cover;
+            position:absolute;
+        }
+        .title{
+            color: #EEE;
+            width: 10rem auto;
+            text-align: center;
+        }
+
+        .title p{
+            margin-top:5px;
+            margin-left:5px;
+            font-weight:bold;
+        }
+
+        .seasons{
+            position:absolute;
+            left:88%;
+            font-size:small;
+        }
+        `;
 
     render(){
         const seasonText = `${this.seasons}  Season${this.seasons > 1 ? 's' : ''}`
