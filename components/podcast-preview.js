bea05b0dbf14436adca448531513d3852515770f
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-01-06 09:08:25 +0200

    add components in podcast-preview.js file

--- components/podcast-preview.js
+++ components/podcast-preview.js
@@ -1,4 +1,32 @@
-class Component extends HTMLElement{
+import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js'
+import { store } from '../modules/store.js'
+
+class Component extends LitElement{
+    static get properties(){
+        return{
+           key: {type: String},
+           image: {type: String},
+           label: {type: String},
+           seasons: {type: Number},
+        }
+    }
+    render(){
+        const seasonText = `${this.seasons}  Season${this.seasons > 1 ? 's' : ''}`
+        return html `
+            <div>
+                <div class="image">
+                    <img src="${this.image}">
+                </div>
+
+                <div class="title">
+                    <p>${this.label}</p>
+                </div>
+
+                <div class="seasons">
+                    <p>{seasonText}</p>
+                </div>
+            </div>`
+    }
 
     connectedCallback(){
         this.innerHTML = `<div>podcast-app</div>`
